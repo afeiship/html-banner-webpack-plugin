06d5ef91ce499c8b57277b1d5af121f07a6300f6
afeiship <1290657123@qq.com>
2020-05-20 19:11:03 +0800

    feat: support html-webpack-plugin 4.x

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var DEFAULT_OPTIONS = {
 
 // import packages:
 require('@feizheng/next-nice-comments');
+require('@feizheng/next-npm-installed');
 
 /**
  * Configure the plugin
@@ -16,22 +17,38 @@ function HtmlBannerWebpackPlugin(inOptions) {
   this.options = objectAssign(DEFAULT_OPTIONS, inOptions);
 }
 
+
+
 /**
  * Implement the plugin
  * webpack-plugin: v3.2.0
  */
-HtmlBannerWebpackPlugin.prototype.apply = function(compiler) {
+HtmlBannerWebpackPlugin.prototype.apply = function (compiler) {
+  if (!nx.npmInstalled('html-webpack-plugin')) {
+    console.log('INFO: html-webpack-plugin required!');
+    return null;
+  }
+
+  var HtmlWebpackPlugin = require('html-webpack-plugin');
   var options = this.options;
+  var process = function (data, cb) {
+    data.html = nx.niceComments(options.banner, options.type) + data.html;
+    cb(null, data)
+  };
+
   if (compiler.hooks) {
-    compiler.plugin('compilation', function(compilation) {
-      compilation.plugin('html-webpack-plugin-before-html-processing', function(data, cb) {
-        data.html = nx.niceComments(options.banner, options.type) + data.html;
-        // cb(null, data)
-      });
+    compiler.plugin('compilation', function (compilation) {
+      // html-webpack-plugin === 4.x
+      if (typeof HtmlWebpackPlugin.getHooks === 'function') {
+        HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tapAsync('HtmlBannerWebpackPlugin', process)
+      } else {
+        // html-webpack-plugin ===  3.x
+        compilation.plugin('html-webpack-plugin-before-html-processing', process);
+      }
     });
   } else {
     // webpack < 4.0:
-    compiler.plugin('emit', function(compilation) {
+    compiler.plugin('emit', function (compilation) {
       console.log('NOT SUPPORT!');
       // updateOptions(compilation);
       // return process(self.options);
